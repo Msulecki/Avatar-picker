072ccf1758b0b257e54ef6cecaa759c5d747d0ab
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-03-28 16:42:52 +0100

    Updates

---
+++ precache-manifest.bd1ff178d2dfd5bd3fdbb2b47ceb0d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80de7bab5e8b78cccd6973f9ea4c2c65",
+    "url": "/Avatar-picker/index.html"
+  },
+  {
+    "revision": "b3111ff595b522530c7c",
+    "url": "/Avatar-picker/static/css/main.c2c3bd93.chunk.css"
+  },
+  {
+    "revision": "42013c2e2238ae28c715",
+    "url": "/Avatar-picker/static/js/2.c061c1a1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Avatar-picker/static/js/2.c061c1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3111ff595b522530c7c",
+    "url": "/Avatar-picker/static/js/main.d5f3a994.chunk.js"
+  },
+  {
+    "revision": "526adc005b51b1b51214",
+    "url": "/Avatar-picker/static/js/runtime-main.24351cac.js"
+  }
+]);
